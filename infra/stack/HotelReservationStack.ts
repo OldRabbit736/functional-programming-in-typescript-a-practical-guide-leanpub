c6bc415ed3b54f86ff7c5786345d83363cfd079e
jspark <sylvan0212@gmail.com>
2021-10-02 19:21:15 +0900

    start!

--- infra/stack/HotelReservationStack.ts
+++ infra/stack/HotelReservationStack.ts
@@ -1,6 +1,6 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 
-export class App3HotelReservationStack extends cdk.Stack {
+export class HotelReservationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
