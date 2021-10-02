394881e6bf151f11b8fe0a72ba6926e515493cd9
jspark <sylvan0212@gmail.com>
2021-10-02 23:37:33 +0900

    finished pdf page 141

--- lambdas/create/src/anticorruption.ts
+++ lambdas/create/src/anticorruption.ts
@@ -4,28 +4,37 @@ import {
 } from "../../../util/domain/types";
 
 import * as E from "fp-ts/lib/Either";
-import { flow } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/function";
+import { Ord } from "fp-ts/lib/Date";
+import { lt } from "fp-ts/lib/Ord";
+import { Json, JsonRecord, parse } from "fp-ts/lib/Json";
 
-const toCreateReservationRequest = (
-  event: LambdaEvent
-): CreateReservationRequest => {
-  const parsedBody: any = JSON.parse(event.body);
+const getBody = (event: LambdaEvent) => event.body;
+
+const toCreateReservationRequest = (body: Json): CreateReservationRequest => {
+  const record = body as JsonRecord;
 
   return {
-    hotelId: parsedBody.hotelId,
-    userId: parsedBody.userId,
-    start: new Date(Date.parse(parsedBody.start)),
-    end: new Date(Date.parse(parsedBody.end)),
-    timestamp: parsedBody.timestamp,
+    hotelId: record.hotelId as string,
+    userId: record.userId as string,
+    start: new Date(Date.parse(record.start as string)),
+    end: new Date(Date.parse(record.end as string)),
+    timestamp: new Date(Date.parse(record.timestamp as string)),
   };
 };
 
 const checkCreateRequest = (createRequest: CreateReservationRequest) =>
-  createRequest.end > createRequest.start
+  lt(Ord)(createRequest.start, createRequest.end)
     ? E.right(createRequest)
     : E.left("Start date should be before end date");
 
-export const fromEventToCreateRequest = flow(
-  toCreateReservationRequest,
-  checkCreateRequest
-);
+export const fromEventToCreateRequest = (
+  e: LambdaEvent
+): E.Either<string, CreateReservationRequest> =>
+  pipe(
+    getBody(e),
+    parse,
+    E.mapLeft((_) => "Could not parse body of request"),
+    E.map(toCreateReservationRequest),
+    E.chain(checkCreateRequest)
+  );
